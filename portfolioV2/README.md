3a4b3968e12120717a354b9fbcba682b80285fb8
kevin <brinslykevin@gmail.com>
2023-05-28 23:24:45 +0530

    Add files via upload

---
+++ portfolioV2/README.md
@@ -0,0 +1,1 @@
+# kevinbrinsly.github.io
---
+++ portfolioV2/js/script.js
@@ -0,0 +1,22 @@
+var typed = new Typed(".multiple-text", {
+  strings: [" Software Engineer"," Front-end Developer"," Back-end Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 100,
+  loop: true
+})
+
+
+
+
+
+// /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+// function myFunction() {
+//   var x = document.getElementById("myLinks") ;
+//   console.log(x);
+//   if (x.style.display === "none") {
+//     x.style.display = "inline";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
