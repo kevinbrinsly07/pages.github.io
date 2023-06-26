1c38acf2a4d6f3844c91c3f352d7b6f3609fd092
kevinbrinsly <brinslykevin@gmail.com>
2023-06-26 12:12:47 +0530

    change fonts

--- portfolioV2/js/script.js
+++ portfolioV2/js/script.js
@@ -7,16 +7,3 @@ var typed = new Typed(".multiple-text", {
 })
 
 
-
-
-
-// /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-// function myFunction() {
-//   var x = document.getElementById("myLinks") ;
-//   console.log(x);
-//   if (x.style.display === "none") {
-//     x.style.display = "inline";
-//   } else {
-//     x.style.display = "block";
-//   }
-// }
